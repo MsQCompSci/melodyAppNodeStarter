31381c1e6d75a0da9d3537bc92573b1454ea679d
MsQCompSci <46223372+MsQCompSci@users.noreply.github.com>
2024-04-10 06:08:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,21 @@ let app = express();
 //Initialize file system module
 let fs = require('fs');
 
-// API endpoint to get list of songs
+// API endpoint to get folder with song files using folder path
 app.get('/songs', (req, res) => {
+  // File directory endpoint to get list of song file names
+  //in the songs folder
   fs.readdir('songs', (err, files) => {
     if (err) {
+      // Account for errors
       res.status(500).send('Error reading song files');
     } else {
+      // Read file names as a JSON object
       res.json({ files });
     }
   });
 });
+
 
 // Set the port used for server traffic.
 let port = 3000;
