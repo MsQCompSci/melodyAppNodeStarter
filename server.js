fec507015e87aa95a07c1e38968d15907066f37b
MsQCompSci <46223372+MsQCompSci@users.noreply.github.com>
2024-04-10 14:46:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-//import express
+//Import express
 let express = require('express');
 
 // Create the Express app
 let app = express();
 
-//Initialize file system module
-let fs = require('fs');
-
-// API endpoint to get folder with song files using folder path
-app.get('/songs', (req, res) => {
-  // File directory endpoint to get list of song file names
-  //in the songs folder
-  fs.readdir('songs', (err, files) => {
-    if (err) {
-      // Account for errors
-      res.status(500).send('Error reading song files');
-    } else {
-      // Read file names as a JSON object
-      res.json({ files });
-    }
-  });
-});
-
-
 // Set the port used for server traffic.
 let port = 3000;
 
-// Serve static files from 'public' directory
+// Middleware to serve static files from 'public' directory
 app.use(express.static('public'));
 
-//run server
+//Initialize file system module
+let fs = require('fs');
+
+// Middleware to serve static files from 'songs' directory
+app.use(express.static('songs'));
+
+// Middleware for parsing JSON files
+app.use(express.json());
+
+// API endpoint to get songs folder
+app.get('/songs', (req, res) => {
+ // File directory endpoint to get list of file names
+ fs.readdir('./songs', (err, files) => {
+   if (err) {
+     // Throws an error - if no file found, etc
+     res.status(500).send('Error reading song files');
+   } else {
+     // Read file names into a JSON object
+     res.json({files});
+   }
+ });
+});
+
+//Run server at port
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+ console.log(`Server running at http://localhost:${port}`);
 });
